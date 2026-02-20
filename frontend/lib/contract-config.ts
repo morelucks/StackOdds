cddde1a470b4be8fc9d2ee0482bcce00e3c1a640
morelucks <luckykamshak@gmail.com>
2026-02-20 05:15:53 +0100

    chore: update mainnet contract configuration

--- frontend/lib/contract-config.ts
+++ frontend/lib/contract-config.ts
@@ -1,7 +1,7 @@
 export const CONTRACT_CONFIG = {
   mainnet: {
-    address: 'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR',
-    name: 'stackodds-v1'
+    address: 'SP34MN3DMM07BNAWYJSHTS4B08T8JRVK8AT810X1B',
+    name: 'stackodds-market-v3'
   },
   testnet: {
     address: 'ST2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR',
